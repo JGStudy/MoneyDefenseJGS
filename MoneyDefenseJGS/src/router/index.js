040612d1f55588379a9ddf316864afec4aea2487
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-09 13:30:57 +0900

    Feat: 공통 AppLayoutPage.vue 생성 및 App.vue 수정

--- MoneyDefenseJGS/src/router/index.js
+++ MoneyDefenseJGS/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '../pages/HomPage.vue'
+import HomePage from '@/pages/start/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
