23a01a38e11ed081da9672ae531d574f875c5291
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-09 10:22:01 +0900

    Docs: 컴포넌트 App.vue 적용

--- MoneyDefenseJGS/src/main.js
+++ MoneyDefenseJGS/src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import './assets/main.css'
 import App from './App.vue'
 import router from './router'
 import '@/assets/reset.css'
 
 const app = createApp(App)
 
+app.use(router)
 app.use(createPinia())
 app.mount('#app')
