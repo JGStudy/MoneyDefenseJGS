50c2e3db2dbd1014d80349af1ec78fd6d9cd9af0
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-09 14:54:46 +0900

    Design: BaseBox.vue tailwind class 수정

--- MoneyDefenseJGS/src/components/common/BaseBox.vue
+++ MoneyDefenseJGS/src/components/common/BaseBox.vue
@@ -1,15 +1,6 @@
 <template>
-  <!-- 
-    w-[343px] => width: 343px
-    rounded-md => border-radius: 8px
-    border border-kbUi08 => border: 1px solid #DDE1E4
-    bg-white => background: #FFFFFF
-    shadow-md => box-shadow: 0px 4px 6px 0px #00000012
-    p-[20px] => padding: 20px 
-    gap-[13px] => gap: 13px 
-  -->
   <div
-    class="w-[343px] rounded-md border border-kbUi08 bg-white shadow-md p-[20px] gap-[13px]"
+    class="w-full rounded-md border-kb-ui-08 bg-white shadow-card p-[20px] gap-[13px] mt-5 mb-5"
     :class="customClass"
   >
     <slot></slot>
