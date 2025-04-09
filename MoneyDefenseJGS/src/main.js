029ee29141b378099c974e1f9b24926c21e05da8
[Github <ghkd2113@gmail.com>
2025-04-09 15:29:49 +0900

    Feat: 거래 등록 js와 ts

--- MoneyDefenseJGS/src/main.js
+++ MoneyDefenseJGS/src/main.js
@@ -1,13 +1,9 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-import '@/assets/reset.css'
-
+import { createPinia } from 'pinia'
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(router)
-
+app.use(createPinia())
 app.mount('#app')
+
--- MoneyDefenseJGS/src/router/index.js
+++ MoneyDefenseJGS/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomPage.vue'
+import TransactionCreatePage from '@/pages/TransactionCreatePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomePage,
     },
+    {
+      path: '/transaction/create',
+      name: 'TransactionCreate',
+      component: TransactionCreatePage,
+    },
   ],
 })
-
 export default router
