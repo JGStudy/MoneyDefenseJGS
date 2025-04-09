7c0c9fd9880f678472c57fe0d176b7d547a76f76
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-10 03:13:41 +0900

    Feat: Onboarding 페이지 메인 기능 구현

--- MoneyDefenseJGS/src/pages/start/OnboardingPage.vue
+++ MoneyDefenseJGS/src/pages/start/OnboardingPage.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="mx-1 font-sans h-screen">
+  <div class="pt-24 pt-mx-1 font-sans relative h-screen">
     <p class="font-bold text-title02 mb-2">지갑을 지켜라</p>
     <p class="font-body01">지금부터 지갑지켜진기스를</p>
-    <p class="font-body01">시작하지</p>
-    <StartButton @click="startHome" label="시작하기" customClass="absolute bottom-0 " />
+    <p class="font-body01 mb-5">시작하지</p>
+    <InputUser v-model="userName" label="자기소개 좀" placeholder="누구 지갑 지켜야지?" />
+    <StartButton
+      @click="handleSubmit"
+      label="시작하기"
+      customClass="absolute bottom-[50px]"
+      :disabled="isInvalid"
+    />
   </div>
 </template>
 
 <script setup>
 import StartButton from '@/components/common/BaseButton.vue'
+import InputUser from '@/components/form/TextInputField.vue'
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { createUser } from '@/api/userApi'
+import { useUserStore } from '@/stores/userStore'
 
 const router = useRouter()
-const startHome = () => {
+const userName = ref('')
+const userStore = useUserStore()
+
+const handleSubmit = async () => {
+  // if (!userName.value) return alert('닉네임을 입력하세요!')
+
+  const newUser = await createUser(userName.value)
+  userStore.setUser(newUser)
+
+  document.cookie = `userId=${newUser.id}; path=/;` // 쿠키 저장
+
   router.push('/')
 }
 </script>
