7f3e7fb72fcbefa53a4c30ec41e4ea0ea677931f
김윤지 <kkamisister1207@gmail.com>
2025-04-09 12:38:48 +0900

    Feat: TransactionPage 라우팅

--- MoneyDefenseJGS/src/router/index.js
+++ MoneyDefenseJGS/src/router/index.js
@@ -1,13 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomPage.vue'
+import TransactionPage from '@/pages/TransactionPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      // 홈 탭
       path: '/',
       name: 'home',
       component: HomePage,
+    },
+
+    {
+      // 가계부 탭
+      path: '/transaction',
+      name: 'transaction',
+      component: TransactionPage,
     },
   ],
 })
