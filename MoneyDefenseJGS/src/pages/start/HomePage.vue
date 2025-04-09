dc71bfbc2adba52d513093e43daf79ab95d65c72
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-09 13:52:10 +0900

    Docs: pages/start 폴더 생성 및 하연 파트 페이지 컨포넌트 위치 변경

---
+++ MoneyDefenseJGS/src/pages/start/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>지갑지켜진기스</h2>
+  <Card />
+  <Button customClass="xi-exchange xi-1x rotate-90 text-kb-ui-03" editContent="보드 편집" />
+</template>
+
+<script setup>
+import Card from '@/components/common/BaseBox.vue'
+import Button from '@/components/button/EditButton.vue'
+</script>
---
+++ MoneyDefenseJGS/src/pages/start/OnboardingPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>지갑지켜진기스를 시작하지.</div>
+</template>
+
+<script setup></script>
