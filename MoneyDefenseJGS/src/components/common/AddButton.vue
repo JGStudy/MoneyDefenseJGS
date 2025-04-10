7869200bcefbd730152a18d7e4709eca367524f7
김윤지 <kkamisister1207@gmail.com>
2025-04-11 06:42:07 +0900

    Fix: AddButton 컴포넌트 css 수정, 라우팅

--- MoneyDefenseJGS/src/components/common/AddButton.vue
+++ MoneyDefenseJGS/src/components/common/AddButton.vue
@@ -1,18 +1,26 @@
 <template>
-  <button
-    class="fixed bottom-[108px] right-[20px] w-12 h-12 rounded-full bg-kb-ui-03 shadow-card flex items-center justify-center z-[60]"
-    @click="$emit('click')"
+  <div
+    class="fixed bottom-[108px] left-1/2 -translate-x-1/2 z-50 w-full max-w-[375px] flex justify-end pr-5"
   >
-    <svg class="w-6 h-6 stroke-kb-ui-11" viewBox="0 0 24 24" fill="none">
-      <path d="M12 5v14M5 12h14" stroke-width="2" stroke-linecap="round" />
-    </svg>
-  </button>
+    <button
+      class="w-12 h-12 rounded-full bg-kb-ui-03 shadow-card flex items-center justify-center z-[60]"
+      @click="goToCreatePage"
+    >
+      <svg class="w-6 h-6 stroke-kb-ui-11" viewBox="0 0 24 24" fill="none">
+        <path d="M12 5v14M5 12h14" stroke-width="2" stroke-linecap="round" />
+      </svg>
+    </button>
+  </div>
 </template>
 
 <script setup>
-const emit = defineEmits(['click'])
+import { useRouter, useRoute } from 'vue-router'
 
-function handleClick() {
-  emit('click')
+const router = useRouter()
+const route = useRoute()
+
+const goToCreatePage = () => {
+  console.log('버튼 클릭됨! 이동 중...')
+  router.push('/transaction/create')
 }
 </script>
