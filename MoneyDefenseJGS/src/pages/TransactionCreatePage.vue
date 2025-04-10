813c996131d55a219a9ea8535ad518fdc0289b2a
[Github <ghkd2113@gmail.com>
2025-04-10 16:47:16 +0900

    Feat: TransactionCreatePage.vue 수정

--- MoneyDefenseJGS/src/pages/TransactionCreatePage.vue
+++ MoneyDefenseJGS/src/pages/TransactionCreatePage.vue
@@ -2,12 +2,7 @@
   <div class="space-y-6 p-4 max-w-xl mx-auto">
     <div>
       <label class="font-semibold">거래처</label>
-      <input
-        v-model="store.partner"
-        type="text"
-        placeholder="입력하세요"
-        class="w-full border rounded px-3 py-2"
-      />
+      <input v-model="store.partner" type="text" placeholder="입력하세요" class="w-full border rounded px-3 py-2" />
     </div>
 
     <TransactionSummary @click="() => alert('요약 클릭됨!')" />
@@ -23,6 +18,4 @@ import EditableTransactionInformation from '@/components/EditableTransactionInfo
 import SaveTransactionButton from '@/components/SaveTransactionButton.vue'
 
 const store = useTransactionStore()
-
-store.$reset?.()
 </script>
