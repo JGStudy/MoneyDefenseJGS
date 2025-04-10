3d349c257e9463a87c1ec5b48b492998f65a7603
BOOK-N6JF33997F\User <happy4u@kakao.com>
2025-04-11 01:04:29 +0900

    Feat: 테마바꾸는 기능 전역 적용 기능 일단 주서거리

--- MoneyDefenseJGS/src/App.vue
+++ MoneyDefenseJGS/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full max-w-[375px] mx-auto px-4 relative" :class="{ dark: isDark }">
+  <div class="w-full max-w-[375px] mx-auto px-4 relative">
     <!-- <AppLayout /> -->
     <router-view />
   </div>
@@ -7,17 +7,17 @@
 
 <script setup>
 import { onMounted } from 'vue'
-import { useThemeStore } from '@/stores/themeStore'
+// import { useThemeStore } from '@/stores/themeStore'
 import { storeToRefs } from 'pinia'
 
-const themeStore = useThemeStore()
-const { isDark } = storeToRefs(themeStore)
+// const themeStore = useThemeStore()
+// const { isDark } = storeToRefs(themeStore)
 
-onMounted(() => {
-  themeStore.initTheme()
-})
+// onMounted(() => {
+//   themeStore.initTheme()
 </script>
-<style>
+<!-- 테마 토글 다 삭제 -->
+<!-- <style>
 body {
   font-family: 'Pretendard', sans-serif;
   background-color: #f7f7f8; /* kb-ui-10 */
@@ -28,4 +28,4 @@ html.dark body {
   background-color: #1c1f24; /* kb-dark-bg */
   color: #f2f4f8; /* kb-dark-text */
 }
-</style>
+</style> -->
