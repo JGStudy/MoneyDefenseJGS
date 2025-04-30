afd2611d9ddbc09d25e7a843a62af30fce6f1d61
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-30 09:26:00 +0900

    Design: ConfirmPopup full screen으로 수정

--- MoneyDefenseJGS/src/components/popup/ConfirmPopup.vue
+++ MoneyDefenseJGS/src/components/popup/ConfirmPopup.vue
@@ -1,34 +1,36 @@
 <template>
-  <div
-    v-if="visible"
-    class="fixed inset-0 z-50 bg-kb-ui-01/50 flex items-center justify-center font-sans"
-  >
-    <div class="w-[327px] bg-kb-ui-11 rounded-t-[2px] flex flex-col overflow-hidden">
-      <!-- message -->
-      <div class="px-6 pt-[30px] pb-[28px] flex flex-col gap-[19px]">
-        <h2 class="text-title03 font-bold">알림</h2>
-        <p class="text-body02 font-light text-kb-ui-03">
-          {{ message }}
-        </p>
-      </div>
+  <teleport to="body">
+    <div
+      v-if="visible"
+      class="fixed inset-0 z-50 bg-kb-ui-01/50 flex items-center justify-center font-sans"
+    >
+      <div class="w-[327px] bg-kb-ui-11 rounded-t-[2px] flex flex-col overflow-hidden">
+        <!-- message -->
+        <div class="px-6 pt-[30px] pb-[28px] flex flex-col gap-[19px]">
+          <h2 class="text-title03 font-bold">알림</h2>
+          <p class="text-body02 font-light text-kb-ui-03">
+            {{ message }}
+          </p>
+        </div>
 
-      <!-- button -->
-      <div class="flex h-[52px]">
-        <button
-          class="w-[130px] bg-kb-ui-09 text-body02 font-bold text-center rounded-bl-[2px]"
-          @click="$emit('cancel')"
-        >
-          {{ cancelText }}
-        </button>
-        <button
-          class="flex-1 bg-[#373D5C] text-white text-body02 font-bold text-center rounded-br-[2px]"
-          @click="$emit('confirm')"
-        >
-          {{ confirmText }}
-        </button>
+        <!-- button -->
+        <div class="flex h-[52px]">
+          <button
+            class="w-[130px] bg-kb-ui-09 text-body02 font-bold text-center rounded-bl-[2px]"
+            @click="$emit('cancel')"
+          >
+            {{ cancelText }}
+          </button>
+          <button
+            class="flex-1 bg-[#373D5C] text-white text-body02 font-bold text-center rounded-br-[2px]"
+            @click="$emit('confirm')"
+          >
+            {{ confirmText }}
+          </button>
+        </div>
       </div>
     </div>
-  </div>
+  </teleport>
 </template>
 
 <script setup>
