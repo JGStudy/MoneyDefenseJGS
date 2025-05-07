68928d9108ea3719987cd1ea50fde6bd54cec576
sangeune <sangeun504@naver.com>
2025-05-07 16:33:48 +0900

    Refactor: 자산/예산 페이지 리팩토링

--- MoneyDefenseJGS/src/api/assetApi.js
+++ MoneyDefenseJGS/src/api/assetApi.js
@@ -3,18 +3,31 @@ import axios from './apiClient'
 // 자산 가져오기 (GET)
 export const fetchAsset = (userId) => axios.get(`/asset?userId=${userId}`)
 
-// 자산 수정 (PATCH)
-export function updateAssetById(assetId, newAmount) {
+export async function updateAssetById(userId, newAmount) {
   const today = new Date().toISOString().split('T')[0]
-  return axios.patch(`/asset/${assetId}`, {
-    totalAsset: newAmount,
-    lastModified: today,
-  })
+
+  // 1. 동일 날짜 + 유저 ID로 데이터 존재하는지 확인
+  const res = await axios.get(`/asset?userId=${userId}&lastModified=${today}`)
+  const existing = res.data[0]
+
+  if (existing) {
+    // 2. 있으면 PATCH로 덮어쓰기
+    return axios.patch(`/asset/${existing.id}`, {
+      totalAsset: newAmount,
+    })
+  } else {
+    // 3. 없으면 POST로 새로 추가
+    return axios.post('/asset', {
+      userId,
+      totalAsset: newAmount,
+      lastModified: today,
+    })
+  }
 }
 
 // 거래 내역 가져오기
 export function fetchTransactions() {
-  return axios.get('/transactions')
+  return axios.get('/Transaction')
 }
 
 //하연 사용
