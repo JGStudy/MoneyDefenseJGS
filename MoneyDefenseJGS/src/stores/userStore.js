561539765f2051fe0ef95bcf27a681daa941ff5b
saym010116 <saym0116@hanyang.ac.kr>
2025-05-10 17:29:00 +0900

    Refactor: 트랜잭션페이지 관련 수정사항 추가 - userid가 새로고침시에도 복원됨(깨지는 현상 수정)

--- MoneyDefenseJGS/src/stores/userStore.js
+++ MoneyDefenseJGS/src/stores/userStore.js
@@ -3,9 +3,24 @@ import { ref } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
   const user = ref(null)
+
   const setUser = (data) => {
     user.value = data
   }
 
-  return { user, setUser }
+  // 저장용 함수는 따로!
+  const saveUserIdToStorage = () => {
+    if (user.value?.id) {
+      localStorage.setItem('userId', user.value.id)
+    }
+  }
+
+  const initUser = () => {
+    const savedId = localStorage.getItem('userId')
+    if (savedId) {
+      user.value = { id: savedId }
+    }
+  }
+
+  return { user, setUser, initUser, saveUserIdToStorage }
 })
