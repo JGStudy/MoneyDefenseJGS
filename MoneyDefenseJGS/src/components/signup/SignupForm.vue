3f90136bda2145ea7dff79c7e15194f7e16d96e8
김윤지 <kkamisister1207@gmail.com>
2025-05-12 15:05:01 +0900

    Feat: id 유효성 검사

--- MoneyDefenseJGS/src/components/signup/SignupForm.vue
+++ MoneyDefenseJGS/src/components/signup/SignupForm.vue
@@ -8,10 +8,24 @@
       :showValidation="touched.nickname"
       @input="touched.nickname = true"
     />
+      <TextInputField
+        v-model="userId"
+        label="아이디"
+        placeholder="아이디를 입력하세요"
+        :validator="validateUserId"
+        :showValidation="touched.userId"
+        @input="
+          () => {
+            touched.userId = true
+            isIdChecked = false
+          }
+        "
+      />
 </template>
 <script setup>
 import TextInputField from '@/components/common/TextInputField.vue'
 const nickname = ref('')
+const userId = ref('')
 // 닉네임
 const validateNickname = (val) => {
   const regex = /^[가-힣a-zA-Z]{2,20}$/ // 길이 2~20자, 한글/영문만 허용
@@ -21,5 +35,14 @@ const validateNickname = (val) => {
     // 금지어 포함 여부 확인
     return { isValid: false, message: '사용할 수 없는 닉네임 입니다. ' }
   return { isValid: true, message: '사용 가능한 닉네임 입니다.' } // 유효
+}
+
+// 아이디
+const validateUserId = (val) => {
+  const regex = /^[a-zA-Z0-9]{5,20}$/ // 영문/숫자 5~20자, 특수문자X
+  if (!regex.test(val)) return { isValid: false, message: '5~20자 영문/숫자만 입력' } // 형식 불일치
+  if (!isIdChecked.value) return { isValid: false, message: '중복 확인 필요' } // 중복 확인 안됨
+  if (isUserIdDuplicate.value) return { isValid: false, message: '이미 사용 중인 아이디 입니다.' } // 중복됨
+  return { isValid: true, message: '사용 가능한 아이디 입니다.' } // 유효
 }
 </script>
