eec7685ac774c262f701583c08f77f9efb553369
김윤지 <kkamisister1207@gmail.com>
2025-05-12 10:22:38 +0900

    Feat: SignupPage, SignupForm 기본 구조 생성

---
+++ MoneyDefenseJGS/src/components/signup/SignupForm.vue
@@ -0,0 +1,8 @@
+<template>
+  <TextInputField></TextInputField>
+  <SignupButton></SignupButton>
+</template>
+<script setup>
+import TextInputField from '@/components/common/TextInputField.vue'
+import SignupButton from '@/components/common/BaseButton.vue'
+</script>
