c9b847479050277220654a9f850ae6ff1bb09733
김윤지 <kkamisister1207@gmail.com>
2025-05-12 09:50:18 +0900

    Feat: 전체 사용자 목록 get 요청 함수 getAllUsers 생성

--- MoneyDefenseJGS/src/api/userApi.js
+++ MoneyDefenseJGS/src/api/userApi.js
@@ -10,3 +10,13 @@ export const createUser = async (userName) => {
   const response = await axios.post('/Profile', { name: userName })
   return response.data
 }
+
+/**
+ * 전체 사용자 목록을 가져오는 함수
+ * @returns {Promise<AxiosResponse>} 모든 사용자 데이터가 담긴 객체
+ */
+export const getAllUsers = () => {
+  return axios.get('/Profile')
+}
+
+
