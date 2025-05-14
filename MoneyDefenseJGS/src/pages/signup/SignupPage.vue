48b407c6d207fd47686afe1f1c38d7adda321e62
김윤지 <kkamisister1207@gmail.com>
2025-05-14 11:50:12 +0900

    Design: SingupPage.vue UI 수정 - 회원가입 완료 버튼 하단 고정 - 회원가입 타이틀 삽입

--- MoneyDefenseJGS/src/pages/signup/SignupPage.vue
+++ MoneyDefenseJGS/src/pages/signup/SignupPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex flex-col items-center px-6 py-10 font-sans">
+  <div class="mx-6 pt-24 pt-mx-1 font-sans relative h-screen">
+    <!-- <p class="text-body01 mb-2">💰지갑을 지켜라!</p> -->
+    <p class="font-bold text-title02 mb-10">회원가입</p>
     <!-- 회원 정보 입력 영역 -->
     <SignupForm @update:formValid="onFormValidUpdate" @update:formValues="onFormValuesUpdate" />
 
@@ -7,6 +9,7 @@
     <SignupButton
       label="회원가입 완료"
       :disabled="!isFormValid"
+      customClass="absolute bottom-[50px]"
       class="mt-6"
       @click="handleSignup"
     />
